b9c717883eb2967820bf910c1e818ecfe7be74c6
unknown <ankitagrawal11b@gmail.com>
2018-12-26 14:12:06 +0530

    Empty Controllers

--- controllers/WelcomeCtrl.js
+++ controllers/WelcomeCtrl.js
@@ -6,14 +6,5 @@ module.exports = {
 
         });
     },
-
-    logout: function* (next) {
-        var sessionId = this.cookies.get("SESSION_ID");
-        if(sessionId) {
-            sessionUtils.deleteSession(sessionId);
-        }
-        this.cookies.set("SESSION_ID", '', {expires: new Date(1), path: '/'});
-
-        this.redirect('/');
-    }
+    
 }
